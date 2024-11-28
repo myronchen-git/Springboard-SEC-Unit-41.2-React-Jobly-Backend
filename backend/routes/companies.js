f9126606b0101bdfe11f77a50ac5df1de84e4e0e
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2024-11-28 12:37:51 -0800

    style: apply formatting to companies.js
    Location:
    backend/routes/companies.js
    
    Created this separate commit in preparation for next commit and to
    declutter the different kinds of changes.

--- backend/routes/companies.js
+++ backend/routes/companies.js
@@ -1,20 +1,19 @@
-"use strict";
+'use strict';
 
 /** Routes for companies. */
 
-const jsonschema = require("jsonschema");
-const express = require("express");
+const jsonschema = require('jsonschema');
+const express = require('express');
 
-const { BadRequestError } = require("../expressError");
-const { ensureAdmin } = require("../middleware/auth");
-const Company = require("../models/company");
+const { BadRequestError } = require('../expressError');
+const { ensureAdmin } = require('../middleware/auth');
+const Company = require('../models/company');
 
-const companyNewSchema = require("../schemas/companyNew.json");
-const companyUpdateSchema = require("../schemas/companyUpdate.json");
-const companySearchSchema = require("../schemas/companySearch.json");
+const companyNewSchema = require('../schemas/companyNew.json');
+const companyUpdateSchema = require('../schemas/companyUpdate.json');
+const companySearchSchema = require('../schemas/companySearch.json');
 
 const router = new express.Router();
-
 
 /** POST / { company } =>  { company }
  *
@@ -25,11 +24,11 @@ const router = new express.Router();
  * Authorization required: admin
  */
 
-router.post("/", ensureAdmin, async function (req, res, next) {
+router.post('/', ensureAdmin, async function (req, res, next) {
   try {
     const validator = jsonschema.validate(req.body, companyNewSchema);
     if (!validator.valid) {
-      const errs = validator.errors.map(e => e.stack);
+      const errs = validator.errors.map((e) => e.stack);
       throw new BadRequestError(errs);
     }
 
@@ -51,7 +50,7 @@ router.post("/", ensureAdmin, async function (req, res, next) {
  * Authorization required: none
  */
 
-router.get("/", async function (req, res, next) {
+router.get('/', async function (req, res, next) {
   const q = req.query;
   // arrive as strings from querystring, but we want as ints
   if (q.minEmployees !== undefined) q.minEmployees = +q.minEmployees;
@@ -60,7 +59,7 @@ router.get("/", async function (req, res, next) {
   try {
     const validator = jsonschema.validate(q, companySearchSchema);
     if (!validator.valid) {
-      const errs = validator.errors.map(e => e.stack);
+      const errs = validator.errors.map((e) => e.stack);
       throw new BadRequestError(errs);
     }
 
@@ -79,7 +78,7 @@ router.get("/", async function (req, res, next) {
  * Authorization required: none
  */
 
-router.get("/:handle", async function (req, res, next) {
+router.get('/:handle', async function (req, res, next) {
   try {
     const company = await Company.get(req.params.handle);
     return res.json({ company });
@@ -99,11 +98,11 @@ router.get("/:handle", async function (req, res, next) {
  * Authorization required: admin
  */
 
-router.patch("/:handle", ensureAdmin, async function (req, res, next) {
+router.patch('/:handle', ensureAdmin, async function (req, res, next) {
   try {
     const validator = jsonschema.validate(req.body, companyUpdateSchema);
     if (!validator.valid) {
-      const errs = validator.errors.map(e => e.stack);
+      const errs = validator.errors.map((e) => e.stack);
       throw new BadRequestError(errs);
     }
 
@@ -119,7 +118,7 @@ router.patch("/:handle", ensureAdmin, async function (req, res, next) {
  * Authorization: admin
  */
 
-router.delete("/:handle", ensureAdmin, async function (req, res, next) {
+router.delete('/:handle', ensureAdmin, async function (req, res, next) {
   try {
     await Company.remove(req.params.handle);
     return res.json({ deleted: req.params.handle });
@@ -127,6 +126,5 @@ router.delete("/:handle", ensureAdmin, async function (req, res, next) {
     return next(err);
   }
 });
-
 
 module.exports = router;
