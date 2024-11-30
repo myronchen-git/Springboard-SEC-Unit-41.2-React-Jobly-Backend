19dbfa36b48f0a91c822236d3f20ec3d9a0bfe2e
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2024-11-29 16:32:49 -0800

    doc: correct user object key
    The user object that the backend API returns for a specified user was
    incorrectly described as returning a key called "jobs".  It is actually
    given "applications".

--- backend/models/user.js
+++ backend/models/user.js
@@ -116,8 +116,8 @@ class User {
 
   /** Given a username, return data about user.
    *
-   * Returns { username, first_name, last_name, is_admin, jobs }
-   *   where jobs is { id, title, company_handle, company_name, state }
+   * Returns { username, first_name, last_name, is_admin, applications }
+   *   where applications is { id, title, company_handle, company_name, state }
    *
    * Throws NotFoundError if user not found.
    **/
