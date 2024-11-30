5c51419a98f6f27ce1f5c2365a57a158f0b1eef7
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2024-11-29 16:37:49 -0800

    style: apply formatting to user files in backend
    Applied formatting in preparation for next commit to simplify commit
    changes.
    
    Affected files:
    backend/models/user.js
    backend/routes/users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,19 +1,18 @@
-"use strict";
+'use strict';
 
 /** Routes for users. */
 
-const jsonschema = require("jsonschema");
+const jsonschema = require('jsonschema');
 
-const express = require("express");
-const { ensureCorrectUserOrAdmin, ensureAdmin } = require("../middleware/auth");
-const { BadRequestError } = require("../expressError");
-const User = require("../models/user");
-const { createToken } = require("../helpers/tokens");
-const userNewSchema = require("../schemas/userNew.json");
-const userUpdateSchema = require("../schemas/userUpdate.json");
+const express = require('express');
+const { ensureCorrectUserOrAdmin, ensureAdmin } = require('../middleware/auth');
+const { BadRequestError } = require('../expressError');
+const User = require('../models/user');
+const { createToken } = require('../helpers/tokens');
+const userNewSchema = require('../schemas/userNew.json');
+const userUpdateSchema = require('../schemas/userUpdate.json');
 
 const router = express.Router();
-
 
 /** POST / { user }  => { user, token }
  *
@@ -27,11 +26,11 @@ const router = express.Router();
  * Authorization required: admin
  **/
 
-router.post("/", ensureAdmin, async function (req, res, next) {
+router.post('/', ensureAdmin, async function (req, res, next) {
   try {
     const validator = jsonschema.validate(req.body, userNewSchema);
     if (!validator.valid) {
-      const errs = validator.errors.map(e => e.stack);
+      const errs = validator.errors.map((e) => e.stack);
       throw new BadRequestError(errs);
     }
 
@@ -43,7 +42,6 @@ router.post("/", ensureAdmin, async function (req, res, next) {
   }
 });
 
-
 /** GET / => { users: [ {username, firstName, lastName, email }, ... ] }
  *
  * Returns list of all users.
@@ -51,7 +49,7 @@ router.post("/", ensureAdmin, async function (req, res, next) {
  * Authorization required: admin
  **/
 
-router.get("/", ensureAdmin, async function (req, res, next) {
+router.get('/', ensureAdmin, async function (req, res, next) {
   try {
     const users = await User.findAll();
     return res.json({ users });
@@ -59,7 +57,6 @@ router.get("/", ensureAdmin, async function (req, res, next) {
     return next(err);
   }
 });
-
 
 /** GET /[username] => { user }
  *
@@ -69,15 +66,18 @@ router.get("/", ensureAdmin, async function (req, res, next) {
  * Authorization required: admin or same user-as-:username
  **/
 
-router.get("/:username", ensureCorrectUserOrAdmin, async function (req, res, next) {
-  try {
-    const user = await User.get(req.params.username);
-    return res.json({ user });
-  } catch (err) {
-    return next(err);
+router.get(
+  '/:username',
+  ensureCorrectUserOrAdmin,
+  async function (req, res, next) {
+    try {
+      const user = await User.get(req.params.username);
+      return res.json({ user });
+    } catch (err) {
+      return next(err);
+    }
   }
-});
-
+);
 
 /** PATCH /[username] { user } => { user }
  *
@@ -89,36 +89,42 @@ router.get("/:username", ensureCorrectUserOrAdmin, async function (req, res, nex
  * Authorization required: admin or same-user-as-:username
  **/
 
-router.patch("/:username", ensureCorrectUserOrAdmin, async function (req, res, next) {
-  try {
-    const validator = jsonschema.validate(req.body, userUpdateSchema);
-    if (!validator.valid) {
-      const errs = validator.errors.map(e => e.stack);
-      throw new BadRequestError(errs);
+router.patch(
+  '/:username',
+  ensureCorrectUserOrAdmin,
+  async function (req, res, next) {
+    try {
+      const validator = jsonschema.validate(req.body, userUpdateSchema);
+      if (!validator.valid) {
+        const errs = validator.errors.map((e) => e.stack);
+        throw new BadRequestError(errs);
+      }
+
+      const user = await User.update(req.params.username, req.body);
+      return res.json({ user });
+    } catch (err) {
+      return next(err);
     }
-
-    const user = await User.update(req.params.username, req.body);
-    return res.json({ user });
-  } catch (err) {
-    return next(err);
   }
-});
-
+);
 
 /** DELETE /[username]  =>  { deleted: username }
  *
  * Authorization required: admin or same-user-as-:username
  **/
 
-router.delete("/:username", ensureCorrectUserOrAdmin, async function (req, res, next) {
-  try {
-    await User.remove(req.params.username);
-    return res.json({ deleted: req.params.username });
-  } catch (err) {
-    return next(err);
+router.delete(
+  '/:username',
+  ensureCorrectUserOrAdmin,
+  async function (req, res, next) {
+    try {
+      await User.remove(req.params.username);
+      return res.json({ deleted: req.params.username });
+    } catch (err) {
+      return next(err);
+    }
   }
-});
-
+);
 
 /** POST /[username]/jobs/[id]  { state } => { application }
  *
@@ -127,15 +133,18 @@ router.delete("/:username", ensureCorrectUserOrAdmin, async function (req, res, 
  * Authorization required: admin or same-user-as-:username
  * */
 
-router.post("/:username/jobs/:id", ensureCorrectUserOrAdmin, async function (req, res, next) {
-  try {
-    const jobId = +req.params.id;
-    await User.applyToJob(req.params.username, jobId);
-    return res.json({ applied: jobId });
-  } catch (err) {
-    return next(err);
+router.post(
+  '/:username/jobs/:id',
+  ensureCorrectUserOrAdmin,
+  async function (req, res, next) {
+    try {
+      const jobId = +req.params.id;
+      await User.applyToJob(req.params.username, jobId);
+      return res.json({ applied: jobId });
+    } catch (err) {
+      return next(err);
+    }
   }
-});
-
+);
 
 module.exports = router;
